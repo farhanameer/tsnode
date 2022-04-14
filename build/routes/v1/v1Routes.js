5eb130360f6449dc51f9285e3430f2a6e40d4d6d
gordian <mohammadshehroz558@gmail.com>
2022-04-14 10:20:53 -0400

    Sepation of DTOs and Validators for Web/App and completing of all the CRUD Operations

--- build/routes/v1/v1Routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-const express_1 = __importDefault(require("express"));
-const category_1 = __importDefault(require("./category"));
-const router = express_1.default.Router();
-router.use("/categories", category_1.default);
-module.exports = router;
